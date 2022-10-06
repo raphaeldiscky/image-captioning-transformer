d685485a7d4d86ffdd007454222200b4fd400f64
Raphael Discky <zundria.putra@gmail.com>
2022-10-06 18:34:13 +0700

    Add karpathy split data

--- settings.py
+++ settings.py
@@ -7,13 +7,13 @@ CCN_MODEL = "imagenet"
 # Desired image dimensions
 IMAGE_SIZE = (299, 299)
 # Max vocabulary size
-MAX_VOCAB_SIZE = 15000
+MAX_VOCAB_SIZE = 20000
 # Fixed length allowed for any sequence
 SEQ_LENGTH = 25
 # Dimension for the image embeddings and token embeddings
 EMBED_DIM = 512
 # Number of self-attention heads
-NUM_HEADS = 6
+NUM_HEADS = 8
 # Per-layer units in the feed-forward network
 FF_DIM = 1024
 # Shuffle dataset dim on tf.data.Dataset
@@ -21,19 +21,19 @@ SHUFFLE_DIM = 512
 # Batch size
 BATCH_SIZE = 64
 # Numbers of training epochs
-EPOCHS = 1
+EPOCHS = 10
 
 
-#### COCO dataset
-# Max number train dataset images : 82783
-# Max number valid dataset images : 40504
+# Use karpathy's split of dataset into train, val, test
+# Train = 113287
+# Val = 5000
+# Test = 5000 (remainder)
 REDUCE_DATASET = False
-# Number of train images -> it must be a value between [1, 82783]
-NUM_TRAIN_IMG = 1000
-# Number of valid images -> it must be a value between [1, 40504]
-# N.B. -> IMPORTANT : the number of images of the test set is given by the difference between 40504 and NUM_VALID_IMG values.
-# for instance, with NUM_VALID_IMG = 20000 -> valid set have 20000 images and test set have the last 13432 images.
-NUM_VALID_IMG = 50
+# Number of train images -> it must be a value between [1, 113287]
+NUM_TRAIN_IMG = 113287
+# Number of valid images -> it must be a value between [1, 10000],
+# if NUM_VALID_IMG = 5000, then NUM_TEST_IMG = 5000
+NUM_VALID_IMG = 5000
 # Data augumention on train set
 TRAIN_SET_AUG = True
 # Data augmention on valid set
@@ -45,8 +45,8 @@ TEST_SET = True
 # valid_data_json_path = "COCO_dataset/captions_mapping_valid_english.json"
 # text_data_json_path = "COCO_dataset/text_data_english.json"
 
-train_data_json_path = "COCO_dataset/mapped_captions_train2014.json"
-valid_data_json_path = "COCO_dataset/mapped_captions_val2014.json"
+train_data_json_path = "COCO_dataset/karpathy_train2014_indo.json"
+valid_data_json_path = "COCO_dataset/karpathy_val2014_indo.json"
 text_data_json_path = "COCO_dataset/text_data_indo.json"
 
 # Save training files directory
